4e8468ef614fc0d4c72a2456763667b3b5c89824
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-09-02 21:25:18 +0300

    tesrt

--- app/Components/HorizontalScroll/HoeizontalScroll.tsx
+++ app/Components/HorizontalScroll/HoeizontalScroll.tsx
@@ -14,19 +14,19 @@ const cards: CardType[] = [
     url: "https://plus.unsplash.com/premium_photo-1683134650708-d84ff30080c3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YXdhcmRzfGVufDB8fDB8fHww",
     title: "Bussines Plan Competition",
     id: 1,
-    desc: "Kompetisi Rencana Bisnis pada bulan Oktober sebagai Pre-Event Let’s Grow",
+    desc: "The Business Plan Competition in October as a Pre-Event for Let’s Grow",
   },
   {
     url: "https://images.unsplash.com/photo-1528605105345-5344ea20e269?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRlZCUyMHRhbGt8ZW58MHx8MHx8fDA%3D",
     title: "Business Talk Show",
     id: 2,
-    desc: "Sesi diskusi inspiratif dengan para ahli bisnis.",
+    desc: "Inspiring discussion session with business experts.",
   },
   {
     url: "https://plus.unsplash.com/premium_photo-1661274209157-118069b926f3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlzY3Vzc2lvbnxlbnwwfHwwfHx8MA%3D%3D",
     title: "Bussines Discussion Session",
     id: 3,
-    desc: "Forum untuk berbagi ide dan strategi dalam mengembangkan usaha.",
+    desc: "Forum for sharing ideas and strategies in business development.",
   },
 ];
 
--- app/Components/Text-parallax/TextParallax.tsx
+++ app/Components/Text-parallax/TextParallax.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode, useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { FiArrowUpRight } from "react-icons/fi";
 import styles from "./TextParallax.module.scss"; // Import the Sass module
+import Link from "next/link";
 
 export const TextParallax = () => {
   return (
@@ -12,21 +13,28 @@ export const TextParallax = () => {
         subheading="Let's Grow"
         heading="Accommodate and Connect the Community."
       >
-        <Content2 />
+        <Content />
       </TextParallaxContent>
       <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Let's Grow"
         heading="Facilitate all the needs of the Community."
       >
-        <Content3 />
+        <Content2 />
       </TextParallaxContent>
       <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Let's Grow"
         heading="Assist in the expansion of the Community."
       >
-        <Content />
+        <Content3 />
+      </TextParallaxContent>
+      <TextParallaxContent
+        imgUrl="https://images.unsplash.com/photo-1460925895917-afdab827c52f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGVjb21tZXJjZSUyMHdlYnNpdGV8ZW58MHx8MHx8fDA%3D"
+        subheading="Let's Grow"
+        heading="Digitalization to centralize the business community."
+      >
+        <Content4 />
       </TextParallaxContent>
     </div>
   );
@@ -104,7 +112,7 @@ const Content = () => (
     <div className="content">
       <p>
         Comprehensively accommodating and establishing strong connections between student SMEs and business institutions or companies in Turkey and Indonesia, creating opportunities for collaboration, mentorship, and development. This
-        initiative aims to bridge the gap between aspiring student entrepreneurs and established businesses, encouraging partnerships that can produce mutual benefits, share knowledge, and expand market reach for student-led ventures.it is
+        initiative aims to bridge the gap between aspiring student entrepreneurs and established businesses, encouraging partnerships that can produce mutual benefits, share knowledge, and expand market reach for student-led ventures. It is
         hoped that business can develop rapidly through enhancing the three main Characteristics of Business Growth, namely Connected Growth, Complementary Growth and Continues Growth.
       </p>
       <button>
@@ -139,6 +147,23 @@ const Content3 = () => (
       <button>
         Learn more <FiArrowUpRight className="inline" />
       </button>
+    </div>
+  </div>
+);
+const Content4 = () => (
+  <div className={styles.exampleContent}>
+    <h2>Let&apos;s Grow</h2>
+    <div className="content">
+      <p>
+        Presents &quot;SEPETIMIZ&quot; as a seamless e-commerce platform to showcasing diverse products and services talented Indonesian diaspora students who spread across various regions in Turkey. This initiative aims to make them can
+        more easily connect with customers and fellow sellers. Those who join this platform also get the benefits of technical efficiency and effectiveness of buying and selling, such as the use and selection of expedition services. By
+        maximizing technological transformation in the business world, we can accelerate the pace of expansion in international markets and realize the dream of economic growth of Indonesia and Turkey.
+      </p>
+      <a href="https://sepetimiz.vercel.app" target="_blank" rel="noopener noreferrer" className="flex items-center bg-blue-500 text-white px-4 py-2 rounded">
+        <button>
+          Learn more <FiArrowUpRight className="inline" />
+        </button>
+      </a>
     </div>
   </div>
 );
